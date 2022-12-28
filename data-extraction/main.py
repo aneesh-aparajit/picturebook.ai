b7e1a7ba226cba7d4a1582569a4f8710857bb9ec
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2022-12-29 02:07:13 +0530

    Updated # of books needed

--- data-extraction/main.py
+++ data-extraction/main.py
@@ -14,9 +14,9 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"
 }
 
-print(f"DOWNLOADING TXT FILES OF {len(df)} BOOKS.")
+print(f"\nDOWNLOADING TXT FILES OF {len(df)} BOOKS.\n")
 
-for ix in tqdm(range(len(df))):
+for ix in tqdm(range(5000)):
     data = df.iloc[ix]
     book_id = data["Link"].split("/")[-1]
     url = BASE_LINK + "files/" + book_id
@@ -31,3 +31,5 @@ for ix in tqdm(range(len(df))):
             s = bs(book.content, "html.parser")
             with open(os.path.join(SAVE_DIR, link), "w") as f:
                 f.write(s.prettify())
+
+print(f'\n\t\t\t\t\t\t\t\t\t\t\t\t\t******** COMPLETED *********')
