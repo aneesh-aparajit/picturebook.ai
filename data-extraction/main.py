27844fcbc1c77a042ad6d717bc91eb425f33691f
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2022-12-29 02:03:01 +0530

    updated code for scraping the data.

---
+++ data-extraction/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+import requests
+from lxml import etree
+import os
+
+df = pd.read_csv("../data/metadata/gutenberg_metadata.csv")
+print(df.head())
+
+BASE_LINK = "https://www.gutenberg.org/"
+SAVE_DIR = "../data/txt_files/"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+}
+
+print(f"DOWNLOADING TXT FILES OF {len(df)} BOOKS.")
+
+for ix in tqdm(range(len(df))):
+    data = df.iloc[ix]
+    book_id = data["Link"].split("/")[-1]
+    url = BASE_LINK + "files/" + book_id
+    page = requests.get(url=url, headers=HEADERS)
+    soup = bs(page.content, "html.parser")
+    links = etree.HTML(str(soup))
+    links = links.xpath("*//a/@href")
+
+    for link in links:
+        if link == f"{book_id}.txt":
+            book = page = requests.get(url=url + "/" + link, headers=HEADERS)
+            s = bs(book.content, "html.parser")
+            with open(os.path.join(SAVE_DIR, link), "w") as f:
+                f.write(s.prettify())
