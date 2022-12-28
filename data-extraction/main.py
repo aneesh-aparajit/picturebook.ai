8d9fc21a56a89609581cc305301972621f782cf6
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2022-12-29 02:44:55 +0530

    Added handler for deprecated case.

--- data-extraction/main.py
+++ data-extraction/main.py
@@ -27,9 +27,14 @@ for ix in tqdm(range(len(df))):
 
     for link in links:
         if link == f"{book_id}.txt":
-            book = page = requests.get(url=url + "/" + link, headers=HEADERS)
+            book = requests.get(url=url + "/" + link, headers=HEADERS)
             s = bs(book.content, "html.parser")
             with open(os.path.join(SAVE_DIR, link), "w") as f:
+                f.write(s.prettify())
+        else:
+            book = requests.get(url + '/' + f'{book_id}/old/{book_id}.txt', headers=HEADERS)
+            s = bs(book.content, "html.parser")
+            with open(os.path.join(SAVE_DIR, f'{book_id}.txt'), "w") as f:
                 f.write(s.prettify())
 
 print(f'\n\t\t\t\t\t\t\t\t\t\t\t\t\t******** COMPLETED *********')
