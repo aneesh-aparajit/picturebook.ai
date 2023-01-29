77a2148228766126e1051bc7527c6968330b61eb
Aneesh Aparajit G <84097543+aneesh-aparajit@users.noreply.github.com>
2023-01-29 23:32:15 +0530

    Create config.py

---
+++ 02-tokenizer/config.py
@@ -0,0 +1,5 @@
+ENGLISH_PATH = '../data/data/english'
+TAMIL_PATH = '../data/data/tamil'
+
+ENGLISH_TOKENIZER_PATH = '../tokenizers/english_gpt2_tokenizer/'
+TAMIL_TOKENIZER_PATH = '../tokenizers/tamil_gpt2_tokenizer/'
